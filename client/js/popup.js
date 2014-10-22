91d3439a01162066bf7ca996d848438739279359
itinao <itinao@gmail.com>
2014-10-22 23:43:00 +0900

    index作成

--- client/js/popup.js
+++ client/js/popup.js
@@ -116,3 +116,22 @@ var load = function() {
     desktopCaptureShareInstance.createShortUrl();
   }
 };
+
+
+// clipbord sample
+//function saveToClipboard(str) {
+//    var textArea = document.createElement("textarea");
+//    textArea.style.cssText = "position:absolute;left:-100%";
+//
+//    document.body.appendChild(textArea);
+//
+//    textArea.value = str;
+//    textArea.select();
+//    document.execCommand("copy");
+//
+//    document.body.removeChild(textArea);
+//}
+//
+//$("#copy-button").click(function() {
+//  saveToClipboard($('#url').text());
+//});
