2f07403628e79fdb39cde82847eacd5c506cef0e
itinao <itinao@gmail.com>
2014-10-28 14:21:27 +0900

    configを外部ファイルにするテスト

---
+++ config/config.js
@@ -0,0 +1,9 @@
+var config = {
+  peerConfig: {
+    host: 'screen-share-signaling.itinao.asia',
+    port: 8080,
+    path: '/share',
+    debug: 2,
+    shareUrl: 'http://screen-share.itinao.asia/share.html'
+  }
+};
